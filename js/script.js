9539100050013cbe34cc4677a780a3b4a167613d
Gunesh Farzaliyev <guneshfarzaliyev@Guneshs-MacBook-Pro.local>
2023-12-15 04:28:00 +0400

    buttons functions were written

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ const blrEl = document.querySelector(".blr");
 const btnRegister = document.querySelector(".btn_register");
 const modalEl = document.querySelector(".modal");
 const closeBtn = document.querySelector(".fa-close");
+
+btnsEl.forEach((btn, indx) => {
+    btn.addEventListener("click", () => {
+      btnsEl.forEach((btnE) => btnE.classList.remove("active"));
+      btn.classList.add("active");
+      modalBody.forEach((mov) => mov.classList.remove("active"));
+      modalBody[indx].classList.add("active");
+    });
+  });
