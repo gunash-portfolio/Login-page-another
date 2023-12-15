c282d251d7a37058e55e6d067a0dca506e454312
Gunesh Farzaliyev <guneshfarzaliyev@Guneshs-MacBook-Pro.local>
2023-12-15 04:37:28 +0400

    javascript part is finished but there are some bug tomorrow i will fix

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-"use strict";
 const btnsEl = document.querySelectorAll(".btn");
 const linksEl = document.querySelectorAll("ul li a");
 const modalBody = document.querySelectorAll(".modal_body");
@@ -7,28 +6,31 @@ const btnRegister = document.querySelector(".btn_register");
 const modalEl = document.querySelector(".modal");
 const closeBtn = document.querySelector(".fa-close");
 
+//===============tabs================
 btnsEl.forEach((btn, indx) => {
-    btn.addEventListener("click", () => {
-      btnsEl.forEach((btnE) => btnE.classList.remove("active"));
-      btn.classList.add("active");
-      modalBody.forEach((mov) => mov.classList.remove("active"));
-      modalBody[indx].classList.add("active");
-    });
-  });
+  btn.addEventListener("click", () => {
+    btnsEl.forEach((btnE) => btnE.classList.remove("active"));
+    btn.classList.add("active");
 
-  btnRegister.addEventListener("click", () => {
-    modalEl.classList.add("active");
-    blrEl.style.display = "block";
+    //=========Modal Body
+    modalBody.forEach((mov) => mov.classList.remove("active"));
+    modalBody[indx].classList.add("active");
   });
+});
 
-  closeBtn.addEventListener("click", () => {
-    modalEl.classList.remove("active");
-    blrEl.style.display = "none";
+btnRegister.addEventListener("click", () => {
+  modalEl.classList.add("active");
+  blrEl.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  modalEl.classList.remove("active");
+  blrEl.style.display = "none";
+});
+
+linksEl.forEach((link) => {
+  link.addEventListener("click", () => {
+    linksEl.forEach((l) => l.classList.remove("active"));
+    link.classList.add("active");
   });
-  
-  linksEl.forEach((link) => {
-    link.addEventListener("click", () => {
-      linksEl.forEach((l) => l.classList.remove("active"));
-      link.classList.add("active");
-    });
-  });
+});
