6c039900e6d8a3b6f3d34bd5a20df3089a246573
Gunesh Farzaliyev <guneshfarzaliyev@Guneshs-MacBook-Pro.local>
2023-12-15 04:18:50 +0400

    All querys selected from html with javascript and  document.querySelector()

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 "use strict";
+const btnsEl = document.querySelectorAll(".btn");
+const linksEl = document.querySelectorAll("ul li a");
+const modalBody = document.querySelectorAll(".modal_body");
+const blrEl = document.querySelector(".blr");
+const btnRegister = document.querySelector(".btn_register");
+const modalEl = document.querySelector(".modal");
+const closeBtn = document.querySelector(".fa-close");
